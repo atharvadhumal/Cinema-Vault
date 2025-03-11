f7148e89ccc504057c1b5e5ab9671e89a41dcde5
Atharva Dhumal <atharvaadhumal@gmail.com>
2025-03-12 00:01:28 +0530

    basic changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-    content: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,jsx}"
+      ],
     theme: {
         extend: {},
         screens: {
